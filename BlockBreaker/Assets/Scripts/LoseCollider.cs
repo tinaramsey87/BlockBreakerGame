081f982643f0e2607c4f74d91f0456947afd50eb
Tina Ramsey <tina.ramsey87@gmail.com>
2020-02-26 13:04:31 -0800

    Add source files for BlockBreaker game created in Unity

---
+++ BlockBreaker/Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseCollider : MonoBehaviour {
+	public LevelManager levelManager;
+
+	void OnTriggerEnter2D(Collider2D trigger) {
+		// levelManager.LoadLevel ("Win");
+		print("Trigger");
+	}
+}
